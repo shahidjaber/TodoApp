9cd5a390634b367460012d1de7bc47c4ccbd0248
shahidjaber <shahidjaber90@gmail.com>
2021-08-24 21:12:16 +0500

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:todoapp/pages/home_page.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   static final String title = 'Todo App';
+
   @override
   Widget build(BuildContext context) => MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: title,
-        theme: ThemeData(
-          primarySwatch: Colors.purple,
-          scaffoldBackgroundColor: Colors.white,
-        ),
-        home: HomePage(),
+          debugShowCheckedModeBanner: false,
+          title: title,
+          theme: ThemeData(
+            primarySwatch: Colors.blueGrey,
+            scaffoldBackgroundColor: Colors.white,
+          ),
+          home: HomePage(),
       );
 }
+
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,24 +2,137 @@ import 'package:flutter/material.dart';
 import '../main.dart';
 
 class HomePage extends StatefulWidget {
-
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    final tabs = [
-      Container(),
-      Container(),
-    ];
+  List<dynamic> list = ["You can add", "You can remove", "You can update"];
 
+  var output = "";
+
+  @override
+  Widget build(BuildContext context) { 
     return Scaffold(
       appBar: AppBar(
         title: Text(MyApp.title),
       ),
+
+      // Bottom Navigation //
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white.withOpacity(0.7),
+        selectedItemColor: Colors.white,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fact_check_outlined),
+            label: 'Todos',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.done, size: 28),
+            label: 'Completed',
+          ),
+        ],
+      ),
+
+      // Body //
+
+      body: ListView.builder(
+          itemCount: list.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.all(10),
+              child: Container(
+                color: Colors.lightBlue[100],
+                height: 50,
+                margin: EdgeInsets.only(top: 12),
+                child: ListTile(
+                  title: Text(
+                    "${list[index]}",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  trailing: Container(
+                    width: 50,
+                    child: Row(
+                      children: [
+                        GestureDetector(
+                            onTap: () {
+                              showDialog(
+                                  context: context,
+                                  builder: (context) {
+                                    return AlertDialog(
+                                      title: Text("Edit Item"),
+                                      content: TextField(
+                                        onChanged: (value) {
+                                          output = value;
+                                        },
+                                      ),
+                                      actions: [
+                                        ElevatedButton(
+                                            onPressed: () {
+                                              setState(() {
+                                                list.replaceRange(
+                                                    index, index + 1, {output});
+                                                Navigator.of(context).pop();
+                                              });
+                                            },
+                                            child: Text("Edit"))
+                                      ],
+                                    );
+                                  });
+                            },
+                            child: Icon(Icons.edit)),
+                        GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                list.removeAt(index);
+                              });
+                            },
+                            child: Icon(Icons.delete)),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            );
+          }),
+
+      floatingActionButton: FloatingActionButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        backgroundColor: Colors.black,
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: Text("Add Item"),
+                content: TextField(
+                  onChanged: (value) {
+                    output = value;
+                    
+                  },
+                ),
+                actions: [
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          list.add(output);
+                          Navigator.of(context).pop();
+                        });
+                      },
+                      child: Text("Add"))
+                ],
+              );
+            },
+          );
+        },
+        child: Icon(Icons.add),
+      ),
     );
-      
   }
 }
